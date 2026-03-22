291a5410d2affbcc06256bf7f50bbe778aa9735a
Amit Saha <justamitsaha@gmail.com>
2026-03-22 08:25:24 +0530

    Fixing angular build issue

--- web/web-app/src/app/app.routes.server.ts
+++ web/web-app/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'practice/:topicId',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'analytics/:topicId',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
